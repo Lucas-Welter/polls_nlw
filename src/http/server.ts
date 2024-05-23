9a6e3bead7b84e17f12854bd00f0eb17e992b0ca
Lucas-Welter <boltwelter@gmail.com>
2024-05-23 16:57:04 -0400

    implementing vote-on-polls, redis and websockets

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,22 +1,26 @@
-import fastify from "fastify"
-import cookie from "@fastify/cookie"
-import { PrismaClient } from "@prisma/client"
-import { z } from "zod"
-import { createPoll } from "./routes/create-poll"
-import { getPoll } from "./routes/get-poll"
-import { voteOnPoll } from "./routes/vote-on-poll"
+import fastify from "fastify";
+import cookie from "@fastify/cookie";
+import websocket from "@fastify/websocket";
+import { createPoll } from "./routes/create-poll";
+import { getPoll } from "./routes/get-poll";
+import { voteOnPoll } from "./routes/vote-on-poll";
+import { pollResults } from "./ws/poll-results";
 
-const app = fastify()
+const app = fastify();
 
 app.register(cookie, {
   secret: "polls-app-nlw",
-  hook: 'onRequest'
-})
+  hook: "onRequest",
+});
 
-app.register(createPoll)
-app.register(getPoll)
-app.register(voteOnPoll)
+app.register(websocket);
+
+app.register(createPoll);
+app.register(getPoll);
+app.register(voteOnPoll);
+
+app.register(pollResults);
 
 app.listen({ port: 3333 }).then(() => {
-  console.log("HTTP server running!")
-})
+  console.log("Server is running!");
+});
